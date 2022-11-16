cd6b1b8da2bec005cf4c7235f6a3409a9352baa4
lap90top <87608561+lap90top@users.noreply.github.com>
2022-11-16 14:04:02 +0530

    Update Company.java

--- src/encapsulation/Company.java
+++ src/encapsulation/Company.java
@@ -6,7 +6,7 @@ public class Company {
 
        // to create object of a class : classname object_name = new classname();
          Employee e = new Employee();
-          // to call method : object_name.method_name
+          // to call method : object_name.method_name();
          e.setEmpId(101);
          System.out.println(e.getEmpId());
 
